7456e8b860a92ecd4891e241551952d589fe0d80
mr-kollie <codewithjustin@yahoo.com>
2024-05-22 13:55:55 -0400

    Updated wsgi.py

--- portfolio/portfolio/wsgi.py
+++ portfolio/portfolio/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
+settings_module = 'portfolio.deployment' if 'WEBSITE_NAME' in os.environ else 'portfolio.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
