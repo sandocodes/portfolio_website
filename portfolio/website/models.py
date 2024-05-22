3b11394a65f71fe02060c5e683a4b7b9a3d6458b
mr-kollie <codewithjustin@yahoo.com>
2024-05-22 13:13:54 -0400

    Views and Models
    Added contents to views.py and models.py files

--- portfolio/website/models.py
+++ portfolio/website/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
 
 # Create your models here.
+
+# category
+class Category(models.Model):
+    # id field will automatically be generated each time a new category is created
+    category_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.category_name
+
+
+# Blog Post
+class BlogPost(models.Model):
+    # id field will automatically be generated each time a new blog post is created
+    title = models.CharField(max_length=255, unique=True)
+    body = CKEditor5Field(null=True, blank=True, config_name='extends')
+    slug = models.SlugField(max_length=255, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateField(auto_now=True)
+    categories = models.ManyToManyField("Category", related_name="posts")
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
+    blog_snippet = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+    
+    def snippet(self):
+        return self.blog_snippet
+
+
+# Programming Language
+class Language(models.Model):
+    prog_language_name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.prog_language_name
+
+
+# Projects
+class Project(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    descritption = CKEditor5Field(null=True, blank=True, config_name='extends')
+    slug = models.SlugField(max_length=255, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateField(auto_now=True)
+    image = models.ImageField(upload_to="projects/", null=True, blank=True)
+    project_url = models.URLField(max_length=255, null=True)
+    languages = models.ManyToManyField("Language", related_name="projects", null=True)
+
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+    def snippet(self):
+        return self.descritption[:100] + "..."
+    
--- portfolio/website/views.py
+++ portfolio/website/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import BlogPost, Project
+
+
 
 # Create your views here.
+def home_page(request):
+    page_name = 'home.html'
+    featured_project = Project.objects.all()[:2]
+    posts = BlogPost.objects.order_by('-date_created')[:3]
+    return render(request, page_name, {'title': 'Welcome Page', 'feat_projects': featured_project, 'posts': posts})
+
+
+# about view
+def about_page(request):
+    # template = loader.get_template('about_page.html')
+    page_name = 'about_page.html'
+    featured_project = Project.objects.all()[:4]
+    return render(request, page_name, {'title': 'About Page', 'feat_projects': featured_project,})
+
+
+# projects view
+def projects_page(request):
+    page_name = 'projects_page.html'
+    projects = Project.objects.all()
+    return render(request, page_name, {'title': 'List of My Work', 'projects': projects})
+
+
+def project_detail_page(request, slug):
+    page_name = 'project_detail.html'
+    project = Project.objects.get(slug=slug)
+    return render(request, page_name, {'title': 'Project Detail', 'project': project})
+
+
+# blog seciton view - Will render blog posts later
+def blog_section_page(request):
+    posts = BlogPost.objects.order_by('-date_created')
+    page_name = 'blog_section.html'
+    return render(request, page_name, {'title': 'Blog - Weekly 5 Minutes Read', 'posts': posts})
+
+
+# Blog Detail Page
+def blog_detail_page(request, slug):
+    page_name = 'blog_detail.html'
+    article = BlogPost.objects.get(slug=slug)
+    return render(request, page_name, {'blog': article, 'title': 'Article Detail'})
+
+
+# Contact seciton view 
+def contact_page(request):
+    page_name = 'contact.html'
+    return render(request, page_name, {'title': 'Blog - Weekly 5 Minutes Read'})
+
