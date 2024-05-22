bf61e9db4c08fcd12065d18804016ffa38670c9d
mr-kollie <codewithjustin@yahoo.com>
2024-05-22 13:16:54 -0400

    Updated admin.py
    Added contents to admin.py file

--- portfolio/website/admin.py
+++ portfolio/website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, BlogPost, Project, Language
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(BlogPost)
+admin.site.register(Project)
+admin.site.register(Language)
