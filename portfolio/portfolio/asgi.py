f81f18cc11925053773dca06125961ea8795d72d
mr-kollie <codewithjustin@yahoo.com>
2024-05-22 13:56:32 -0400

    Update asgi.py

--- portfolio/portfolio/asgi.py
+++ portfolio/portfolio/asgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-settings_module = 'portfolio.deployment' if 'WEBSITE_NAME' in os.environ else 'portfolio.settings'
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
 
 application = get_asgi_application()
