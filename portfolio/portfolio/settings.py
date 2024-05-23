ca5d1643fc261ba1caba168b77f4846785bc579d
Justin <codewithjustin@yahoo.com>
2024-05-22 20:17:49 -0400

    changes made to settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": f"{BASE_DIR} / db.sqlite3",
     }
 }
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
