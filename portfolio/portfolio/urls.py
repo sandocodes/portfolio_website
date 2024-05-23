54afac7e86b6cb98113615f7cb2e2bccbbacdcef
Justin <codewithjustin@yahoo.com>
2024-05-22 23:34:10 -0400

    Update urls.py

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -17,10 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('website.urls')),
     path("admin/", admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
